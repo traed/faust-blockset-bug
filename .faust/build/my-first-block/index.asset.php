1df8aa60122c8b5603f00115b997be65e1f67898
Mattias Forsman <forsman1991@gmail.com>
2024-01-05 11:40:35 +0100

    Add test block

---
+++ .faust/build/my-first-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-hooks', 'wp-style-engine'), 'version' => '4bc426eb15888a7647f8');
---
+++ wp-blocks/my-first-block/MyFirstBlock.jsx
@@ -0,0 +1,23 @@
+import { useBlocksTheme } from "@faustwp/blocks";
+
+export default function MyFirstBlock(props) {
+    // This does not work
+    const theme = useBlocksTheme();
+    const style = getStyles(theme, Object.assign({}, props));
+    const { attributes} = props;
+
+    // This does work
+    // const { style, attributes} = props;
+
+    const styles = {
+      ...style,
+    };
+    const cssClassName = 'create-block-my-first-block';
+    return (
+        <div
+            style={styles}
+            className={cssClassName}
+            dangerouslySetInnerHTML={{ __html: attributes.message }}
+        />
+    );
+}
